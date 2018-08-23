586c3c00beb12469458a5a988416c2dc8afbf744
hlums <honglu@microsoft.com>
2018-08-23 15:56:44 -0400

    Use shared benchmark_paths

--- energy_load/GEFCom2017-D_Prob_MT_hourly/common/extract_data.py
+++ energy_load/GEFCom2017-D_Prob_MT_hourly/common/extract_data.py
@@ -61,26 +61,17 @@ Holiday:
     10: Christmas Day
 """
 
-import os, sys, getopt, inspect
+import os, sys, getopt
 from datetime import timedelta
 
 import pandas as pd
-import numpy as np
+
+from utils import split_train_test
+from benchmark_paths import DATA_DIR, HOLIDAY_DATA_PATH
 
 # This assumes that the script is stored in a directory of the same level
 # as the data directory
-SCRIPT_PATH = os.path.dirname(os.path.abspath(inspect.getfile(
-    inspect.currentframe())))
-DATA_DIR_LEVEL = os.path.dirname(SCRIPT_PATH)
-DATA_DIR = os.path.join(DATA_DIR_LEVEL, 'data')
-TRAIN_DATA_DIR = DATA_DIR + '/train'
-TEST_DATA_DIR = DATA_DIR + '/test'
-# This file stores all the data before 2016-12-01
-TRAIN_BASE_FILE = 'train_base.csv'
-# These files contain data to be added to train_base.csv to form the training
-# data of a particular round
-TRAIN_ROUND_FILE_PREFIX = 'train_round_'
-TEST_ROUND_FILE_PREFIX = 'test_round_'
+FULL_OUTPUT_FILE = 'full_data.csv'
 
 DATA_FILE_LIST = ['2011_smd_hourly.xls', '2012_smd_hourly.xls',
                   '2013_smd_hourly.xls', '2014_smd_hourly.xls',
@@ -94,18 +85,6 @@ MA_ZONE_LIST = ['SEMA', 'WCMA', 'NEMA']
 COLUMN_LIST = ['Date', 'Hour', 'DEMAND', 'DryBulb', 'DewPnt']
 COLUMN_LIST_NEW = ['Date', 'Hr_End', 'RT_Demand', 'Dry_Bulb', 'Dew_Point']
 
-TRAIN_BASE_END = pd.to_datetime('2016-12-01')
-TRAIN_ROUNDS_ENDS = pd.to_datetime(['2016-12-15', '2016-12-31',
-                                    '2017-01-15', '2017-01-31',
-                                    '2017-02-14', '2017-02-28'])
-
-TEST_STARTS_ENDS = [pd.to_datetime(('2017-01-01', '2017-02-01')),
-                    pd.to_datetime(('2017-02-01', '2017-03-01')),
-                    pd.to_datetime(('2017-02-01', '2017-03-01')),
-                    pd.to_datetime(('2017-03-01', '2017-04-01')),
-                    pd.to_datetime(('2017-03-01', '2017-04-01')),
-                    pd.to_datetime(('2017-04-01', '2017-05-01'))]
-
 # These dates are used to correct doubled demand values at the end of DST
 # every year. The problem is fixed starting from 2016. It doesn't worth
 # doing outlier detection for these 5 data points.
@@ -114,11 +93,6 @@ DST_END_DATETIME = pd.to_datetime(['2011-11-06 02:00:00',
                                    '2013-11-03 02:00:00',
                                    '2014-11-02 02:00:00',
                                    '2015-11-01 02:00:00'])
-# Holiday data file path
-HOLIDAY_DATA_DIR_LEVEL = os.path.dirname(os.path.dirname(os.path.dirname(
-    SCRIPT_PATH)))
-HOLIDAY_DATA_FILE = \
-    os.path.join(HOLIDAY_DATA_DIR_LEVEL, 'common', 'us_holidays.csv')
 
 # Holiday dictionary used to map holidays to integers
 HOLIDAY_TO_INT_DICT = {"New Year's Day": 1,
@@ -218,7 +192,7 @@ def parse_excel(file_name):
     return df_final
 
 def preprocess_holiday_data():
-    holidays = pd.read_csv(HOLIDAY_DATA_FILE)
+    holidays = pd.read_csv(HOLIDAY_DATA_PATH)
     holidays['Date'] = pd.to_datetime(holidays['Date'])
     # Map holiday names to integers
     holidays = holidays.replace({'Holiday': HOLIDAY_TO_INT_DICT})
@@ -258,13 +232,6 @@ def main(preprocess_flag):
     # preprocess the holiday data
     holiday_df = preprocess_holiday_data()
 
-    # Create train and test data directories
-    if not os.path.isdir(TRAIN_DATA_DIR):
-        os.mkdir(TRAIN_DATA_DIR)
-
-    if not os.path.isdir(TEST_DATA_DIR):
-        os.mkdir(TEST_DATA_DIR)
-
     file_df_list = []
     for file_name in DATA_FILE_LIST:
         print(file_name)
@@ -288,46 +255,14 @@ def main(preprocess_flag):
         dst_end_datetime_mask = \
             file_df_final['Datetime'].isin(DST_END_DATETIME)
         file_df_final.loc[dst_end_datetime_mask,'DEMAND'] = \
-            round(file_df_final.loc[dst_end_datetime_mask,'DEMAND']/2)
+            round(file_df_final.loc[dst_end_datetime_mask, 'DEMAND']/2)
 
     file_df_final.set_index('Datetime', inplace=True)
     file_df_final = merge_with_holiday_data(file_df_final, holiday_df)
 
-    index_value = file_df_final.index.get_level_values(0)
-    train_base_df = file_df_final.loc[index_value <= TRAIN_BASE_END]
-    train_base_df.to_csv(os.path.join(TRAIN_DATA_DIR, TRAIN_BASE_FILE))
-    print('Base training data frame size: {}'.format(train_base_df.shape))
+    file_df_final.to_csv(os.path.join(DATA_DIR, FULL_OUTPUT_FILE))
 
-    for i in range(len(TRAIN_ROUNDS_ENDS)):
-        file_name = os.path.join(TRAIN_DATA_DIR,
-                                 TRAIN_ROUND_FILE_PREFIX + str(i+1) + '.csv')
-        train_round_delta_df = file_df_final.loc[
-            (index_value > TRAIN_BASE_END)
-            & (index_value <= TRAIN_ROUNDS_ENDS[i])]
-        print('Round {0} additional training data size: {1}'.format(i+1,
-            train_round_delta_df.shape))
-        print('Minimum timestamp: {0}'
-              .format(min(train_round_delta_df.index.get_level_values(0))))
-        print('Maximum timestamp: {0}'
-              .format(max(train_round_delta_df.index.get_level_values(0))))
-        print('')
-        train_round_delta_df.to_csv(file_name)
-
-    for i in range(len(TEST_STARTS_ENDS)):
-        file_name = os.path.join(TEST_DATA_DIR,
-                                 TEST_ROUND_FILE_PREFIX + str(i+1) + '.csv')
-        start_end = TEST_STARTS_ENDS[i]
-        test_round_df = file_df_final.loc[
-            ((index_value > start_end[0]) & (index_value <= start_end[1]))
-        ]
-        print('Round {0} testing data size: {1}'
-              .format(i+1, test_round_df.shape))
-        print('Minimum timestamp: {0}'
-        .format(min(test_round_df.index.get_level_values(0))))
-        print('Maximum timestamp: {0}'
-        .format(max(test_round_df.index.get_level_values(0))))
-        print('')
-        test_round_df.to_csv(file_name)
+    split_train_test(file_df_final, DATA_DIR)
 
 def usage():
     print('usage: python extract_data.py [--preprocess]\n'

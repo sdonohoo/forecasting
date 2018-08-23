23d8444309c449c9935d8d0e4b1a6a06b997b13b
hlums <honglu@microsoft.com>
2018-08-23 15:54:55 -0400

    Moved feature engineering script to common folder.

--- energy_load/GEFCom2017-D_Prob_MT_hourly/common/feature_engineering.py
+++ energy_load/GEFCom2017-D_Prob_MT_hourly/common/feature_engineering.py
@@ -1,20 +1,16 @@
 import os
 import pandas as pd
 
-import localpath
-from benchmark_paths import BENCHMARK_DATA_PATH
 from feature_utils import create_features
 from utils import split_train_test, is_datetime_like
+from benchmark_paths import DATA_DIR
+
+OUTPUT_DIR = os.path.join(DATA_DIR, 'features')
+FULL_DATA_FILE = 'full_data.csv'
+FULL_DATA_PATH = os.path.join(DATA_DIR, FULL_DATA_FILE)
 
 DATETIME_COLNAME = 'Datetime'
 HOLIDAY_COLNAME = 'Holiday'
-FULL_DATA_FILE = 'full_data.csv'
-FULL_DATA_PATH = os.path.join(BENCHMARK_DATA_PATH, FULL_DATA_FILE)
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-OUTPUT_DIR = os.path.join(SCRIPT_DIR, 'data/features')
-
-print(OUTPUT_DIR)
 
 def main(input_file, output_dir, datetime_colname, holiday_colname):
     if not os.path.isdir(output_dir):
@@ -24,10 +20,10 @@ def main(input_file, output_dir, datetime_colname, holiday_colname):
 
     df_features = create_features(df, datetime_colname=datetime_colname,
                                   holiday_colname=holiday_colname)
-    if not is_datetime_like(df_features[DATETIME_COLNAME]):
-        df_features[DATETIME_COLNAME] = \
-            pd.to_datetime(df_features[DATETIME_COLNAME])
-    df_features.set_index(DATETIME_COLNAME, inplace=True)
+    if not is_datetime_like(df_features[datetime_colname]):
+        df_features[datetime_colname] = \
+            pd.to_datetime(df_features[datetime_colname])
+    df_features.set_index(datetime_colname, inplace=True)
     split_train_test(df_features, output_dir)
 
 

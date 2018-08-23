ac3a098ef1dc2ea787296bfe7623bf6d5e255a53
hlums <honglu@microsoft.com>
2018-08-23 15:59:05 -0400

    Minor updates to the directory names.

--- energy_load/GEFCom2017-D_Prob_MT_hourly/submissions/benchmark_paths.py
+++ energy_load/GEFCom2017-D_Prob_MT_hourly/submissions/benchmark_paths.py
@@ -1,13 +1,13 @@
 """
 This file is shared by all submissions to get benchmark paths and add the
-benchmark common folder to sys.path
+benchmark common folder to sys.path.
 """
 import os, sys
 
-SUBMISSIONS_PATH = os.path.dirname(os.path.abspath(__file__))
-BENCHMARK_PATH = os.path.dirname(SUBMISSIONS_PATH)
-BENCHMARK_COMMON_PATH = os.path.join(BENCHMARK_PATH, 'common')
-BENCHMARK_DATA_PATH = os.path.join(BENCHMARK_PATH, 'data')
+SUBMISSIONS_DIR = os.path.dirname(os.path.abspath(__file__))
+BENCHMARK_DIR = os.path.dirname(SUBMISSIONS_DIR)
+BENCHMARK_COMMON_DIR = os.path.join(BENCHMARK_DIR, 'common')
+BENCHMARK_DATA_DIR = os.path.join(BENCHMARK_DIR, 'data')
 
-if BENCHMARK_COMMON_PATH not in sys.path:
-    sys.path.insert(0, BENCHMARK_COMMON_PATH)
+if BENCHMARK_COMMON_DIR not in sys.path:
+    sys.path.insert(0, BENCHMARK_COMMON_DIR)

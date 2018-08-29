437f16101820e1f7a8465623c2ddd6325d3f6e86
hlums <honglu@microsoft.com>
2018-08-29 22:52:58 +0000

    Use joblib to parallelized model training.

--- energy_load/GEFCom2017-D_Prob_MT_hourly/submissions/submission_0/train_score.py
+++ energy_load/GEFCom2017-D_Prob_MT_hourly/submissions/submission_0/train_score.py
@@ -1,8 +1,10 @@
 import os
 from datetime import datetime
+import itertools
 import pandas as pd
 import numpy as np
 from statsmodels.regression.quantile_regression import QuantReg
+from joblib import Parallel, delayed
 
 import localpath
 from benchmark_paths import BENCHMARK_DATA_DIR, SUBMISSIONS_DIR
@@ -35,32 +37,46 @@ def preprocess():
     pass
 
 
-def train_single_group(train_df_single, quantiles):
+def train_single_quantile(train_df_single, q):
     model = QuantReg(train_df_single[TARGET_COL], train_df_single[FEATURE_COLS])
+    model_fit = model.fit(q=q)
+
+    return model_fit
+
+
+def train_single_group(train_df_single, quantiles, group_name):
+    models_all = [train_single_quantile(train_df_single.copy(), q) for q in quantiles]
+
     models_dict = {}
-    for q in quantiles:
-        models_dict['Q' + str(int(q*100))] = model.fit(q=q)
+    for q, m in zip(quantiles, models_all):
+        models_dict['Q' + str(int(q*100))] = m
 
-    return models_dict
+    return group_name, models_dict
 
 
-def score_single_group(test_df_single, model_dict):
+def score_single_group(test_df_single, models_dict):
     output = test_df_single[['Zone', 'Datetime']].copy()
-    for q, m in model_dict.items():
-        res = m.predict(test_df_single[FEATURE_COLS])
-        output[q] = res
+    for q, m in models_dict.items():
+        output[q] = m.predict(test_df_single[FEATURE_COLS])
 
     return output
 
 
-def train(train_df):
-    models_all = train_df.groupby('Zone').apply(lambda g: train_single_group(g, QUANTILES))
+def train(train_df, parallel):
+    train_df_grouped = train_df.groupby('Zone')
 
-    return models_all
+    models_all = parallel\
+        (delayed(train_single_group)(group, QUANTILES, name) for name, group in train_df_grouped)
+
+    models_all_dict = {}
+    for k, v in models_all:
+        models_all_dict[k] = v
+
+    return models_all_dict
 
 
 def score(test_df, models_all):
-    group_names = models_all.index.values
+    group_names = models_all.keys()
 
     predictions_all = []
 
@@ -73,22 +89,28 @@ def score(test_df, models_all):
 
 
 def main():
-    startTime = datetime.now()
+    start_time = datetime.now()
     train_test = serve_folds(TRAIN_DATA_DIR, TEST_DATA_DIR)
     predictions_all = []
-    for train_round, test_round, round_num in train_test:
-        print('Round ' + str(round_num))
-        models_all = train(train_round)
-        predictions_round = score(test_round, models_all)
-        predictions_round['Round'] = round_num
-        predictions_all.append(predictions_round)
+    with Parallel(n_jobs=-1) as parallel:
+        for train_round, test_round, round_num in train_test:
+            print('Round ' + str(round_num))
+            models_all = train(train_round, parallel)
+            predictions_round = score(test_round, models_all)
+            predictions_round['Round'] = round_num
+            predictions_all.append(predictions_round)
 
     predictions_final = pd.concat(predictions_all)
+
+    predictions_final = \
+        predictions_final[['Round', 'Zone', 'Datetime', 'Q10', 'Q20', 'Q30',
+                           'Q40', 'Q50', 'Q60', 'Q70', 'Q80', 'Q90']]
 
     predictions_final.to_csv(RESULT_PATH, index=False)
 
     print('Prediction output size: {0}'.format(predictions_final.shape))
-    print('Running time: {0}'. format(datetime.now() - startTime))
+    print('Running time: {0}'. format(datetime.now() - start_time))
+
 
 if __name__ == '__main__':
     main()

aeb30fad27c0c82dd473d87fc2dc35971ee656f8
hlums <honglu@microsoft.com>
2018-09-14 19:59:24 +0000

    Make feature engineering script independent of common.feature_utils

--- energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/baseline/feature_engineering.py
+++ energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/baseline/feature_engineering.py
@@ -6,12 +6,14 @@ calling the feature_utils functions with alternative parameters.
 """
 import os, sys, getopt
 from functools import reduce
+from datetime import timedelta
+import datetime
+import pandas as pd
+import numpy as np
 
 import localpath
 from energy_load.GEFCom2017_D_Prob_MT_hourly.common.benchmark_paths\
     import DATA_DIR, SUBMISSIONS_DIR
-from common.feature_utils import *
-from common.utils import is_datetime_like
 
 print('Data directory used: {}'.format(DATA_DIR))
 
@@ -27,6 +29,177 @@ DATETIME_COLNAME = 'Datetime'
 HOLIDAY_COLNAME = 'Holiday'
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+
+ALLOWED_TIME_COLUMN_TYPES = [pd.Timestamp, pd.DatetimeIndex,
+                             datetime.datetime, datetime.date]
+
+
+def is_datetime_like(x):
+    return any(isinstance(x, col_type)
+               for col_type in ALLOWED_TIME_COLUMN_TYPES)
+
+
+def hour_of_day(datetime_col):
+    return datetime_col.dt.hour
+
+
+def month_of_year(date_time_col):
+    return date_time_col.dt.month
+
+def fourier_approximation(t, n, period):
+    """
+    Generic helper function for create Fourier Series at different
+    harmonies(n) and periods.
+    """
+    x = n * 2 * np.pi * t/period
+    x_sin = np.sin(x)
+    x_cos = np.cos(x)
+
+    return x_sin, x_cos
+
+
+def annual_fourier(datetime_col, n_harmonics):
+    day_of_year = datetime_col.dt.dayofyear
+
+    output_dict = {}
+    for n in range(1, n_harmonics+1):
+        sin, cos = fourier_approximation(day_of_year, n, 365.24)
+
+        output_dict['annual_sin_'+str(n)] = sin
+        output_dict['annual_cos_'+str(n)] = cos
+
+    return output_dict
+
+
+def weekly_fourier(datetime_col, n_harmonics):
+    day_of_week = datetime_col.dt.dayofweek
+
+    output_dict = {}
+    for n in range(1, n_harmonics+1):
+        sin, cos = fourier_approximation(day_of_week, n, 7)
+
+        output_dict['weekly_sin_'+str(n)] = sin
+        output_dict['weekly_cos_'+str(n)] = cos
+
+    return output_dict
+
+
+def daily_fourier(datetime_col, n_harmonics):
+    hour_of_day = datetime_col.dt.hour + 1
+
+    output_dict = {}
+    for n in range(1, n_harmonics+1):
+        sin, cos = fourier_approximation(hour_of_day, n, 24)
+
+        output_dict['daily_sin_'+str(n)] = sin
+        output_dict['daily_cos_'+str(n)] = cos
+
+    return output_dict
+
+
+def same_week_day_hour_lag(datetime_col, value_col, n_years=3,
+                           week_window=1, agg_func='mean',
+                           output_colname='SameWeekHourLag'):
+    """
+    Create a lag feature by averaging values of and around the same week,
+    same day of week, and same hour of day, of previous years.
+    :param datetime_col: Datetime column
+    :param value_col: Feature value column to create lag feature from
+    :param n_years: Number of previous years data to use
+    :param week_window:
+        Number of weeks before and after the same week to
+        use, which should help reduce noise in the data
+    :param agg_func: aggregation function to apply on multiple previous values
+    :param output_colname: name of the output lag feature column
+    """
+
+    if not is_datetime_like(datetime_col):
+        datetime_col = pd.to_datetime(datetime_col, format=DATETIME_FORMAT)
+    min_time_stamp = min(datetime_col)
+    max_time_stamp = max(datetime_col)
+
+    df = pd.DataFrame({'Datetime': datetime_col, 'value': value_col})
+    df.set_index('Datetime', inplace=True)
+
+    week_lag_base = 52
+    week_lag_last_year = list(range(week_lag_base - week_window,
+                              week_lag_base + week_window + 1))
+    week_lag_all = []
+    for y in range(n_years):
+        week_lag_all += [x + y * 52 for x in week_lag_last_year]
+
+    week_lag_cols = []
+    for w in week_lag_all:
+        if (max_time_stamp - timedelta(weeks=w)) >= min_time_stamp:
+            col_name = 'week_lag_' + str(w)
+            week_lag_cols.append(col_name)
+
+            lag_datetime = df.index.get_level_values(0) - timedelta(weeks=w)
+            valid_lag_mask = lag_datetime >= min_time_stamp
+
+            df[col_name] = np.nan
+
+            df.loc[valid_lag_mask, col_name] = \
+                df.loc[lag_datetime[valid_lag_mask], 'value'].values
+
+    # Additional aggregation options will be added as needed
+    if agg_func == 'mean':
+        df[output_colname] = round(df[week_lag_cols].mean(axis=1))
+
+    return df[[output_colname]]
+
+
+def same_day_hour_lag(datetime_col, value_col, n_years=3,
+                      day_window=1, agg_func='mean',
+                      output_colname='SameDayHourLag'):
+    """
+    Create a lag feature by averaging values of and around the same day of
+    year, and same hour of day, of previous years.
+    :param datetime_col: Datetime column
+    :param value_col: Feature value column to create lag feature from
+    :param n_years: Number of previous years data to use
+    :param day_window:
+        Number of days before and after the same day to
+        use, which should help reduce noise in the data
+    :param agg_func: aggregation function to apply on multiple previous values
+    :param output_colname: name of the output lag feature column
+    """
+
+    if not is_datetime_like(datetime_col):
+        datetime_col = pd.to_datetime(datetime_col, format=DATETIME_FORMAT)
+    min_time_stamp = min(datetime_col)
+    max_time_stamp = max(datetime_col)
+
+    df = pd.DataFrame({'Datetime': datetime_col, 'value': value_col})
+    df.set_index('Datetime', inplace=True)
+
+    day_lag_base = 365
+    day_lag_last_year = list(range(day_lag_base - day_window,
+                                   day_lag_base + day_window + 1))
+    day_lag_all = []
+    for y in range(n_years):
+        day_lag_all += [x + y * 365 for x in day_lag_last_year]
+
+    day_lag_cols = []
+    for d in day_lag_all:
+        if (max_time_stamp - timedelta(days=d)) >= min_time_stamp:
+            col_name = 'day_lag_' + str(d)
+            day_lag_cols.append(col_name)
+
+            lag_datetime = df.index.get_level_values(0) - timedelta(days=d)
+            valid_lag_mask = lag_datetime >= min_time_stamp
+
+            df[col_name] = np.nan
+
+            df.loc[valid_lag_mask, col_name] = \
+                df.loc[lag_datetime[valid_lag_mask], 'value'].values
+
+    # Additional aggregation options will be added as needed
+    if agg_func == 'mean':
+        df[output_colname] = round(df[day_lag_cols].mean(axis=1))
+
+    return df[[output_colname]]
 
 
 def create_basic_features(input_df, datetime_colname):

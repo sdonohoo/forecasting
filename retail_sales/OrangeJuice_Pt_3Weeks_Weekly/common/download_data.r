e4e5c0cfd043a9bde37803e7b7fd339ffba19dea
Ubuntu <chenhui@chhubuntuvm.xhloppv4okve1gznz20b1ucgmh.xx.internal.cloudapp.net>
2018-09-14 21:50:38 +0000

    Added Auto ARIMA method for retail sales forecasting

--- retail_sales/OrangeJuice_Pt_3Weeks_Weekly/common/download_data.r
+++ retail_sales/OrangeJuice_Pt_3Weeks_Weekly/common/download_data.r
@@ -1,7 +1,7 @@
 # Retrieves the orangeJuice dataset from the bayesm R package
 # and saves as csv
 
-install.packages("bayesm")
+install.packages("bayesm", repos = "http://mran.revolutionanalytics.com/snapshot/2018-08-27/")
 
 library(bayesm)
 

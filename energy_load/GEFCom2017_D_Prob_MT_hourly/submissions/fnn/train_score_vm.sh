3cb3b97e4646026b076d10e90a0e8d47d4670a4d
Zhou Fang <zhouf@microsoft.com>
2018-10-31 07:53:54 +0000

    initial submission for fnn model using qrnn package

---
+++ energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/fnn/train_score_vm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+path=energy_load/GEFCom2017_D_Prob_MT_hourly
+for i in `seq 1 5`;
+do
+    echo "Run $i"
+    start=`date +%s`
+    echo 'Creating features...'
+    python $path/submissions/fnn/feature_engineering.py --submission fnn
+
+    echo 'Training and predicting...'
+    Rscript $path/submissions/fnn/train_predict_qrnn.R $i
+
+    end=`date +%s`
+    echo 'Running time '$((end-start))' seconds'
+done

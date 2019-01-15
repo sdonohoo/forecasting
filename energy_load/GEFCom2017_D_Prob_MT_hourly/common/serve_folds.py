e2612e80670b2bf871f10e6e0514bc4d96f636e9
Vanja Paunic <vapaunic@microsoft.com>
2019-01-15 13:21:46 +0000

    minor comments for serve_folds.py

--- energy_load/GEFCom2017_D_Prob_MT_hourly/common/serve_folds.py
+++ energy_load/GEFCom2017_D_Prob_MT_hourly/common/serve_folds.py
@@ -2,13 +2,16 @@ import os, sys, inspect
 import pandas as pd
 
 def serve_folds():
+    """
+    This script generates train and test data for each forecast round on the fly
+    """
+    
     from benchmark_paths import DATA_DIR
     TRAIN_DATA_DIR = DATA_DIR + '/train'
     TEST_DATA_DIR = DATA_DIR + '/test'
     # This file stores all the data before 2016-12-01
     TRAIN_BASE_FILE = 'train_base.csv'
-    # These files contain data to be added to train_base.csv to form the training
-    # data of a particular round
+    # These files contain data to be added to train_base.csv to form the training data of a particular round
     TRAIN_ROUND_FILE_PREFIX = 'train_round_'
     TEST_ROUND_FILE_PREFIX = 'test_round_'
 
@@ -30,12 +33,12 @@ def serve_folds():
 
         yield train_round_df, test_round_df, i+1
 
-# # Test serve_folds
-# for train, test, _ in serve_folds():
-#     print('Training data size: {}'.format(train.shape))
-#     print('Testing data size: {}'.format(test.shape))
-#     print('Minimum training timestamp: {}'.format(min(train['Datetime'])))
-#     print('Maximum training timestamp: {}'.format(max(train['Datetime'])))
-#     print('Minimum testing timestamp: {}'.format(min(test['Datetime'])))
-#     print('Maximum testing timestamp: {}'.format(max(test['Datetime'])))
-#     print('')
+# Test serve_folds
+for train, test, _ in serve_folds():
+    print('Training data size: {}'.format(train.shape))
+    print('Testing data size: {}'.format(test.shape))
+    print('Minimum training timestamp: {}'.format(min(train['Datetime'])))
+    print('Maximum training timestamp: {}'.format(max(train['Datetime'])))
+    print('Minimum testing timestamp: {}'.format(min(test['Datetime'])))
+    print('Maximum testing timestamp: {}'.format(max(test['Datetime'])))
+    print('')

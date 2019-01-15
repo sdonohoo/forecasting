1b5c3417ee8a087a387f0022a3e85b957139e1c5
Zhou Fang <zhouf@microsoft.com>
2019-01-15 09:38:27 +0000

    add timestamp into cv_output files

--- energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/fnn/aml_estimator.py
+++ energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/fnn/aml_estimator.py
@@ -3,6 +3,7 @@ import os
 import sys
 import getopt
 import pandas as pd
+from datetime import datetime
 from azureml.core import Run
 run = Run.get_submitted_run()
 
@@ -10,11 +11,13 @@ base_command = "Rscript train_validate_aml.R"
 
 if __name__ == '__main__':
     opts, args = getopt.getopt(
-        sys.argv[1:], '', ['path=', 'n_hidden_1=', 'n_hidden_2=',
+        sys.argv[1:], '', ['path=', 'cv_path=', 'n_hidden_1=', 'n_hidden_2=',
                            'iter_max=', 'penalty='])
     for opt, arg in opts:
         if opt == '--path':
             path = arg
+        elif opt == '--cv_path':
+            cv_path = arg
         elif opt == '--n_hidden_1':
             n_hidden_1 = arg
         elif opt == '--n_hidden_2':
@@ -23,18 +26,22 @@ if __name__ == '__main__':
             iter_max = arg
         elif opt == '--penalty':
             penalty = arg
+    time_stamp = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
     task = " ".join([base_command,
                     '--path', path,
+                    '--cv_path', cv_path,
                     '--n_hidden_1', n_hidden_1,
                     '--n_hidden_2', n_hidden_2,
                     '--iter_max', iter_max,
-                    '--penalty', penalty])
+                    '--penalty', penalty,
+                    '--time_stamp', time_stamp])
     process = subprocess.call(task, shell=True)
 
     # process.communicate()
     # process.wait()
-
-    result = pd.read_csv(os.path.join(path, 'cv_output.csv'))
+    
+    output_file_name = 'cv_output_' + time_stamp + '.csv'
+    result = pd.read_csv(os.path.join(cv_path, output_file_name))
 
     APL = result['loss'].mean()
 

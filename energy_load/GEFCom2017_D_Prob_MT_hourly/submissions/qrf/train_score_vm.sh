b11453b59918493db9a651ec58614b4d2e65918c
pechyony <pechyony@outlook.com>
2019-01-16 16:56:21 -0500

    training files

---
+++ energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/qrf/train_score_vm.sh
@@ -0,0 +1,14 @@
+path=energy_load/GEFCom2017_D_Prob_MT_hourly
+for i in `seq 1 5`;
+do
+    echo "Run $i"
+    start=`date +%s`
+    echo 'Creating features...'
+    python $path/submissions/qrf/feature_engineering.py --submission qrf
+
+    echo 'Training and predicting...'
+    python $path/submissions/qrf/train_score.py --data-folder . --seed $i
+
+    end=`date +%s`
+    echo 'Running time '$((end-start))' seconds'
+done

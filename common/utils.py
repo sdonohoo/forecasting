1d703b24943451da95e71be82a0f2b95ae216bfa
Vanja Paunic <vapaunic@microsoft.com>
2019-01-16 12:06:13 +0000

    documented utils.py

--- common/utils.py
+++ common/utils.py
@@ -7,6 +7,7 @@ ALLOWED_TIME_COLUMN_TYPES = [pd.Timestamp, pd.DatetimeIndex,
 
 
 def is_datetime_like(x):
+    """Function that checks if a data frame column x is of a datetime type."""
     return any(isinstance(x, col_type)
                for col_type in ALLOWED_TIME_COLUMN_TYPES)
 
@@ -15,6 +16,17 @@ def get_datetime_col(df, datetime_colname):
     """
     Helper function for extracting the datetime column as datetime type from
     a data frame.
+
+    Args:
+        df: pandas DataFrame containing the column to convert
+        datetime_colname: name of the column to be converted
+    
+    Returns:
+        datetime_col: converted column
+
+    Raises:
+        Exception: if datetime_colname does not exist in the dateframe df.
+        Exception: if datetime_colname cannot be converted to datetime type.
     """
     if datetime_colname in df.index.names:
         datetime_col = df.index.get_level_values(datetime_colname)
@@ -36,7 +48,7 @@ def get_datetime_col(df, datetime_colname):
 
 def get_month_day_range(date):
     """
-    Return the first date and last date of the month of the given date.
+    Returns the first date and last date of the month of the given date.
     """
     # Replace the date in the original timestamp with day 1
     first_day = date + relativedelta(day=1)
@@ -49,14 +61,15 @@ def get_month_day_range(date):
 
 def split_train_validation(df, fct_horizon, datetime_colname):
     """
-    Split the input dataframe into train and validate folds based
-    on the forecast creation time(fct) and forecast horizon specified
-    by fct_horizon.
-    :param df: The whole input data frame.
-    :param fct_horizon: list of tuples in the format of (fct, (forecast_horizon_start, forecast_horizon_end))
-    :param datetime_colname: name of the datetime column
-    Note:
-        df[datetime_colname] needs to be a datetime type.
+    Splits the input dataframe into train and validate folds based on the forecast creation time (fct) and forecast horizon specified by fct_horizon.
+    
+    Args:
+        df: The input data frame to split.
+        fct_horizon: list of tuples in the format of 
+            (fct, (forecast_horizon_start, forecast_horizon_end))
+        datetime_colname: name of the datetime column
+    
+    Note: df[datetime_colname] needs to be a datetime type.
     """
     i_round = 0
     for fct, horizon in fct_horizon:
@@ -68,6 +81,22 @@ def split_train_validation(df, fct_horizon, datetime_colname):
 
 
 def add_datetime(input_datetime, unit, add_count):
+    """
+    Function to add a specified units of time (years, months, weeks, days, hours, or minutes) to the input datetime.
+
+    Args:
+        input_datetime: datatime to be added to
+        unit: unit of time, valid values: 'year', 'month', 'week',
+            'day', 'hour', 'minute'.
+        add_count: number of units to add
+    
+    Returns:
+        new_datetime: the resulting datetime
+    
+    Raises:
+        Exception: if invalid unit is provided. Valid units are:
+            'year', 'month', 'week', 'day', 'hour', 'minute'.
+    """
     if unit == 'year':
         new_datetime = input_datetime + relativedelta(years=add_count)
     elif unit == 'month':
@@ -81,6 +110,5 @@ def add_datetime(input_datetime, unit, add_count):
     elif unit == 'minute':
         new_datetime = input_datetime + relativedelta(minutes=add_count)
     else:
-        raise Exception('Invalid backtest step unit, {}, provided. Valid step units are'
-                        'year, month, week, day, hour, and minute'.format(unit))
+        raise Exception('Invalid backtest step unit, {}, provided. Valid step units are year, month, week, day, hour, and minute'.format(unit))
     return new_datetime

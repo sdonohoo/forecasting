b9faa1813a457ef5bc787fb5175fdca68162dc46
root <root@chhdlvmlinux.dfp2unry0csu3ece5dcebwl4gd.jx.internal.cloudapp.net>
2019-01-24 19:05:01 +0000

    Added hyperparameter tuning scripts

--- retail_sales/OrangeJuice_Pt_3Weeks_Weekly/submissions/LightGBM/train_score.py
+++ retail_sales/OrangeJuice_Pt_3Weeks_Weekly/submissions/LightGBM/train_score.py
@@ -4,7 +4,6 @@
 # Train and score a boosted decision tree model using [LightGBM Python package](https://github.com/Microsoft/LightGBM) from Microsoft, 
 # which is a fast, distributed, high performance gradient boosting framework based on decision tree algorithms.  
 
-
 import os
 import sys
 import math
@@ -20,122 +19,8 @@ tsperf_dir = os.getcwd()
 if tsperf_dir not in sys.path:
     sys.path.append(tsperf_dir)
 
-from common.metrics import MAPE
+from make_features import make_features
 import retail_sales.OrangeJuice_Pt_3Weeks_Weekly.common.benchmark_settings as bs
-
-# Get random seed
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--seed', type=int, help='Random seed of GRM algorithm')
-args = parser.parse_args()
-seed = args.seed
-print('Random seed is {}'.format(seed))
-
-# Data paths
-DATA_DIR = os.path.join(tsperf_dir, 'retail_sales', 'OrangeJuice_Pt_3Weeks_Weekly', 'data')
-SUBMISSION_DIR = os.path.join(tsperf_dir, 'retail_sales', 'OrangeJuice_Pt_3Weeks_Weekly', 'submissions', 'LightGBM')
-TRAIN_DIR = os.path.join(DATA_DIR, 'train')
-TEST_DIR = os.path.join(DATA_DIR, 'test')
-
-# Parameters of GBM model
-params = {
-    'num_leaves': 50, 
-    'objective': 'mape', 
-    'min_data_in_leaf': 200, 
-    'learning_rate': 0.002, 
-    'feature_fraction': 0.9, 
-    'bagging_fraction': 0.7,
-    'bagging_freq': 1,
-    'num_threads': 16,
-    'seed': seed,
-    'early_stopping_rounds': 125
-}
-MAX_ROUNDS = 400 #100 
-
-# Lags and categorical features
-lags = [2,3,4] 
-categ_fea = ['store', 'brand', 'deal', 'feat'] 
-
-# Utility functions
-def week_of_month(dt):
-    """Get the week of the month for the specified date.
-    
-    Args: 
-        dt (Datetime): Input date
-        
-    Returns:
-        wom (Integer): Week of the month of the input date
-    """ 
-    from math import ceil
-    first_day = dt.replace(day=1)
-    dom = dt.day
-    adjusted_dom = dom + first_day.weekday()
-    wom = int(ceil(adjusted_dom/7.0))
-    return wom
-
-def df_from_cartesian_product(dict_in):
-    """Generate a Pandas dataframe from Cartesian product of lists.
-    
-    Args: 
-        dict_in (Dictionary): Dictionary containing multiple lists
-        
-    Returns:
-        df (Dataframe): Dataframe corresponding to the Caresian product of the lists
-    """
-    from collections import OrderedDict
-    from itertools import product
-    od = OrderedDict(sorted(dict_in.items()))
-    cart = list(product(*od.values()))
-    df = pd.DataFrame(cart, columns=od.keys())
-    return df
-
-def lagged_features(df, lags):
-    """Create lagged features based on time series data.
-    
-    Args:
-        df (Dataframe): Input time series data sorted by time
-        lags (List): Lag lengths
-        
-    Returns:
-        fea (Dataframe): Lagged features 
-    """
-    df_list = []
-    for lag in lags:
-        df_shifted = df.shift(lag)
-        df_shifted.columns = [x + '_lag' + str(lag) for x in df_shifted.columns]
-        df_list.append(df_shifted)
-    fea = pd.concat(df_list, axis=1)
-    return fea
-
-def moving_averages(df, start_step, window_size=None):
-    """Compute averages of every feature over moving time windows.
-    
-    Args:
-        df (Dataframe): Input features as a dataframe
-    
-    Returns:
-        fea (Dataframe): Dataframe consisting of the moving averages
-    """
-    if window_size == None: # Use a large window to compute average over all historical data
-        window_size = df.shape[0]
-    fea = df.shift(start_step).rolling(min_periods=1, center=False, window=window_size).mean()
-    fea.columns = fea.columns + '_mean'
-    return fea
-
-def create_features(df, lags):
-    """Create features used for model training.
-    
-    Args:
-        df (Dataframe): Time series data of a certain store and brand
-    
-    Returns:
-        fea_all (Dataframe): All features for the specific store and brand
-    """
-    lagged_fea = lagged_features(df[['move']], lags)
-    moving_avg = moving_averages(df[['move']], 2, 10)
-    fea_columns = ['brand', 'store', 'week', 'week_of_month', 'day', 'deal', 'feat', 'move']
-    #fea_columns = fea_columns + ['price1', 'price2', 'price3', 'price4', 'price5', 'price6', 'price7', 'price8', 'price9', 'price10', 'price11']
-    fea_all = pd.concat([df[fea_columns], lagged_fea, moving_avg], axis=1)
-    return fea_all
 
 def make_predictions(df, model):
     """Predict sales with the trained GBM model.
@@ -151,80 +36,92 @@ def make_predictions(df, model):
     predictions['move'] = predictions['move'].apply(lambda x: round(x))
     return pd.concat([df[['brand', 'store', 'week']].reset_index(drop=True), predictions], axis=1)
 
-def evaluate(result):
-    """Compute MAPE value of the forecast.
-    
-    Args:
-        result (Dataframe): Input dataframe including predicted sales and actual sales
-    
-    Returns:
-        MAPE value of the forecast
-    """
-    return MAPE(result['move'], result['actual'])*100
+if __name__ == '__main__':
+    # Parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, dest='seed', default=1, help='Random seed of GBM model')
+    parser.add_argument('--num-leaves', type=int, dest='num_leaves', default=32, help='# of leaves of the tree')
+    parser.add_argument('--min-data-in-leaf', type=int, dest='min_data_in_leaf', default=50, help='mini # of samples in each leaf')
+    parser.add_argument('--learning-rate', type=float, dest='learning_rate', default=0.001, help='learning rate')
+    parser.add_argument('--feature-fraction', type=float, dest='feature_fraction', default=1.0, help='ratio of features used in each iteration')
+    parser.add_argument('--bagging-fraction', type=float, dest='bagging_fraction', default=1.0, help='ratio of samples used in each iteration')
+    parser.add_argument('--bagging-freq', type=int, dest='bagging_freq', default=1, help='bagging frequency')
+    parser.add_argument('--max-rounds', type=int, dest='max_rounds', default=400, help='# of boosting iterations')
+    parser.add_argument('--max-lag', type=int, dest='max_lag', default=5, help='max lag of unit sales')
+    parser.add_argument('--max-moving-step', type=int, dest='max_moving_step', default=5, help='max step for computing moving average of unit sales')
+    args = parser.parse_args()
+    print(args)
 
-# Train and predict for all forecast rounds
-pred_all = []
-metric_all = []
-for r in range(bs.NUM_ROUNDS): 
-    print('---- Round ' + str(r+1) + ' ----')
-    train_df = pd.read_csv(os.path.join(TRAIN_DIR, 'train_round_'+str(r+1)+'.csv'))
-    train_df['move'] = train_df['logmove'].apply(lambda x: round(math.exp(x)))
-    train_df = train_df[['store', 'brand', 'week', 'profit', 'move']]
-    # Fill missing values
+    # Data paths
+    DATA_DIR = os.path.join(tsperf_dir, 'retail_sales', 'OrangeJuice_Pt_3Weeks_Weekly', 'data')
+    SUBMISSION_DIR = os.path.join(tsperf_dir, 'retail_sales', 'OrangeJuice_Pt_3Weeks_Weekly', 'submissions', 'LightGBM')
+    TRAIN_DIR = os.path.join(DATA_DIR, 'train')
+
+    # Parameters of GBM model
+    params = {
+        'num_leaves': 113, #50, 
+        'objective': 'mape', 
+        'min_data_in_leaf': 500, #200, 
+        'learning_rate': 0.1, #0.002, 
+        'feature_fraction': 0.28, #0.9, 
+        'bagging_fraction': 0.7,
+        'bagging_freq': 18, #1,
+        'num_threads': 16,
+        'early_stopping_rounds': 125,
+        'seed': args.seed
+    }
+    MAX_ROUNDS = 1720 #400 
+
+    # Lags and categorical features
+    lags = np.arange(2, 19+1)
+    used_columns = ['store', 'brand', 'week', 'week_of_month', 'month', 'deal', 'feat', 'move', 'price', 'price_ratio']
+    categ_fea = ['store', 'brand', 'deal'] 
+
+    # Get unique stores and brands
+    train_df = pd.read_csv(os.path.join(TRAIN_DIR, 'train_round_1.csv'))
     store_list = train_df['store'].unique()
     brand_list = train_df['brand'].unique()
-    week_list = range(bs.TRAIN_START_WEEK, bs.TEST_END_WEEK_LIST[r]+1)
-    d = {'store': store_list,
-         'brand': brand_list,
-         'week': week_list}        
-    data_grid = df_from_cartesian_product(d)
-    data_filled = pd.merge(data_grid, train_df, how='left', 
-                            on=['store', 'brand', 'week'])
-    aux_df = pd.read_csv(os.path.join(TRAIN_DIR, 'aux_round_'+str(r+1)+'.csv'))  
-    data_filled = pd.merge(data_filled, aux_df, how='left',  # Get future price, deal, and advertisement info
-                            on=['store', 'brand', 'week'])
-    data_filled = data_filled.groupby(['store', 'brand']).apply(lambda x: x.fillna(method='ffill').fillna(method='bfill'))
-    # Create datetime features
-    data_filled['week_start'] = data_filled['week'].apply(lambda x: bs.FIRST_WEEK_START + datetime.timedelta(days=(x-1)*7))
-    data_filled['year'] = data_filled['week_start'].apply(lambda x: x.year)
-    data_filled['month'] = data_filled['week_start'].apply(lambda x: x.month)
-    data_filled['week_of_month'] = data_filled['week_start'].apply(lambda x: week_of_month(x))
-    data_filled['day'] = data_filled['week_start'].apply(lambda x: x.day)
-    data_filled.drop('week_start', axis=1, inplace=True)
-    # Create other features (lagged features, moving averages, etc.)
-    features = data_filled.groupby(['store','brand']).apply(lambda x: create_features(x, lags))
-    train_fea = features[features.week <= bs.TRAIN_END_WEEK_LIST[r]].reset_index(drop=True)
-    # Drop rows with NaN values
-    train_fea.dropna(inplace=True)
-    print('Training and predicting models...')
-    dtrain = lgb.Dataset(
-                train_fea.drop('move', axis=1, inplace=False), 
-                label = train_fea['move']
-    )
-    # Train GBM model
-    bst = lgb.train(
-        params, 
-        dtrain, 
-        num_boost_round = MAX_ROUNDS,
-        valid_sets = [dtrain], 
-        categorical_feature = categ_fea,
-        verbose_eval =  False
-    )
-    # Generate forecasts
-    test_fea = features[features.week >= bs.TEST_START_WEEK_LIST[r]].reset_index(drop=True)
-    pred = make_predictions(test_fea, bst).sort_values(by=['store','brand', 'week']).reset_index(drop=True)
-    # Additional columns required by the submission format
-    pred['round'] = r+1
-    pred['weeks_ahead'] = pred['week'] - bs.TRAIN_END_WEEK_LIST[r]
-    # Keep the predictions 
-    pred_all.append(pred)
 
-# Generate submission
-submission = pd.concat(pred_all, axis=0)
-submission.rename(columns={'move': 'prediction'}, inplace=True)
-submission = submission[['round', 'store', 'brand', 'week', 'weeks_ahead', 'prediction']]
-filename = 'submission_seed_' + str(seed) + '.csv'
-submission.to_csv(os.path.join(SUBMISSION_DIR, filename), index=False)
+    # Train and predict for all forecast rounds
+    pred_all = []
+    metric_all = []
+    for r in range(bs.NUM_ROUNDS): 
+        print('---- Round ' + str(r+1) + ' ----')
+        # Create features
+        features = make_features(r, TRAIN_DIR, lags, args.max_moving_step, used_columns, store_list, brand_list)
+
+        train_fea = features[features.week <= bs.TRAIN_END_WEEK_LIST[r]].reset_index(drop=True)
+
+        # Drop rows with NaN values
+        train_fea.dropna(inplace=True)
+
+        print('Training models and making predictions...')
+        dtrain = lgb.Dataset(train_fea.drop('move', axis=1, inplace=False), 
+                            label = train_fea['move'])
+        # Train GBM model
+        bst = lgb.train(
+            params, 
+            dtrain, 
+            num_boost_round = MAX_ROUNDS,
+            valid_sets = [dtrain], 
+            categorical_feature = categ_fea,
+            verbose_eval =  True #False
+        )
+        # Generate forecasts
+        test_fea = features[features.week >= bs.TEST_START_WEEK_LIST[r]].reset_index(drop=True)
+        pred = make_predictions(test_fea, bst).sort_values(by=['store','brand', 'week']).reset_index(drop=True)
+        # Additional columns required by the submission format
+        pred['round'] = r+1
+        pred['weeks_ahead'] = pred['week'] - bs.TRAIN_END_WEEK_LIST[r]
+        # Keep the predictions 
+        pred_all.append(pred)
+
+    # Generate submission
+    submission = pd.concat(pred_all, axis=0)
+    submission.rename(columns={'move': 'prediction'}, inplace=True)
+    submission = submission[['round', 'store', 'brand', 'week', 'weeks_ahead', 'prediction']]
+    filename = 'submission_seed_' + str(seed) + '.csv'
+    submission.to_csv(os.path.join(SUBMISSION_DIR, filename), index=False)
 
 
 

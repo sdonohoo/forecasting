9ebdc0fc6dec85b48032f2262212b631fcc39dd7
root <root@chhdsvmlinux.ok5uym0ls3wuzp0l4yhtsjqocg.xx.internal.cloudapp.net>
2019-01-28 22:43:08 +0000

    Reduced overlap code

--- retail_sales/OrangeJuice_Pt_3Weeks_Weekly/submissions/LightGBM/train_validate.py
+++ retail_sales/OrangeJuice_Pt_3Weeks_Weekly/submissions/LightGBM/train_validate.py
@@ -13,39 +13,7 @@ import pandas as pd
 import lightgbm as lgb 
 from azureml.core import Run
 from sklearn.model_selection import train_test_split
-
-# Utility functions
-def week_of_month(dt):
-    """Get the week of the month for the specified date.
-    
-    Args: 
-        dt (Datetime): Input date
-        
-    Returns:
-        wom (Integer): Week of the month of the input date
-    """ 
-    from math import ceil
-    first_day = dt.replace(day=1)
-    dom = dt.day
-    adjusted_dom = dom + first_day.weekday()
-    wom = int(ceil(adjusted_dom/7.0))
-    return wom
-
-def df_from_cartesian_product(dict_in):
-    """Generate a Pandas dataframe from Cartesian product of lists.
-    
-    Args: 
-        dict_in (Dictionary): Dictionary containing multiple lists
-        
-    Returns:
-        df (Dataframe): Dataframe corresponding to the Caresian product of the lists
-    """
-    from collections import OrderedDict
-    from itertools import product
-    od = OrderedDict(sorted(dict_in.items()))
-    cart = list(product(*od.values()))
-    df = pd.DataFrame(cart, columns=od.keys())
-    return df
+from utils import week_of_month, df_from_cartesian_product
 
 def lagged_features(df, lags):
     """Create lagged features based on time series data.

c289db86b36f13a870acbe0a3194c79f0fafbd73
Hong Lu <honglu@microsoft.com>
2019-03-12 17:34:08 -0400

    Added parallelization to baseline submission.

--- energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/baseline/train_predict.R
+++ energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/baseline/train_predict.R
@@ -2,6 +2,15 @@ args = commandArgs(trailingOnly=TRUE)
 seed_value = args[1]
 library('data.table')
 library('quantreg')
+library('doParallel')
+
+n_cores = detectCores()
+
+cl <- parallel::makeCluster(n_cores)
+parallel::clusterEvalQ(cl, lapply(c("quantreg", "data.table"), library, character.only = TRUE))
+registerDoParallel(cl)
+
+
 data_dir = 'energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/baseline/data/features'
 train_dir = file.path(data_dir, 'train')
 test_dir = file.path(data_dir, 'test')
@@ -11,13 +20,11 @@ test_file_prefix = 'test_round_'
 
 output_file = file.path(paste('energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/baseline/submission_seed_', seed_value, '.csv', sep=""))
 
-normalize_columns = list( 'load_lag', 'dry_bulb_lag')
+normalize_columns = list('load_lag', 'dry_bulb_lag')
 
 quantiles = seq(0.1, 0.9, by = 0.1)
 
 result_all = list()
-counter = 1
-
 for (iR in 1:6){
   print(paste('Round', iR))
   train_file = file.path(train_dir, paste(train_file_prefix, iR, '.csv', sep=''))
@@ -33,40 +40,43 @@ for (iR in 1:6){
     test_df[, c] = (test_df[, ..c] - min_c)/(max_c - min_c)
   }
   
-
-  zones = unique(train_df[, Zone])
-  hours = unique(train_df[, hour_of_day])
   
   test_df$average_load_ratio = rowMeans(test_df[,c('recent_load_ratio_10', 'recent_load_ratio_11', 'recent_load_ratio_12',
                                                  'recent_load_ratio_13', 'recent_load_ratio_14', 'recent_load_ratio_15', 'recent_load_ratio_16')], na.rm=TRUE)
-  
-  
   test_df[, load_ratio:=mean(average_load_ratio), by=list(hour_of_day, month_of_year)]
   
   
-  for (z in zones) {
-    print(paste('Zone', z))
-    for (h in hours){
-      train_df_sub = train_df[Zone == z & hour_of_day == h]
-      test_df_sub = test_df[Zone == z & hour_of_day == h]
+  zones = unique(train_df[, Zone])
+  hours = unique(train_df[, hour_of_day])
+  all_zones_hours = expand.grid(zones, hours)
+  colnames(all_zones_hours) = c('Zone', 'hour_of_day')
+  
+  result_all_zones_hours = foreach(i = 1:nrow(all_zones_hours), .combine = rbind) %dopar%{
+    z = all_zones_hours[i, 'Zone']
+    h = all_zones_hours[i, 'hour_of_day']
+    
+    train_df_sub = train_df[Zone == z & hour_of_day == h]
+    test_df_sub = test_df[Zone == z & hour_of_day == h]
+    
+    result = data.table(Zone=test_df_sub$Zone, Datetime = test_df_sub$Datetime, Round=iR)
+    result_all_quantiles = list()
+    q_counter = 1
+    for (tau in quantiles){
       
-      result = data.table(Zone=test_df_sub$Zone, Datetime = test_df_sub$Datetime, Round=iR)
+      model =  rq(DEMAND ~ load_lag + dry_bulb_lag +
+                    annual_sin_1 + annual_cos_1 + annual_sin_2 + annual_cos_2 + annual_sin_3 + annual_cos_3 +
+                    weekly_sin_1 + weekly_cos_1 + weekly_sin_2 + weekly_cos_2 + weekly_sin_3 + weekly_cos_3,
+                  data=train_df_sub, tau = tau)
       
-      for (tau in quantiles){
-        
-        model =  rq(DEMAND ~ load_lag + dry_bulb_lag +
-                      annual_sin_1 + annual_cos_1 + annual_sin_2 + annual_cos_2 + annual_sin_3 + annual_cos_3 +
-                      weekly_sin_1 + weekly_cos_1 + weekly_sin_2 + weekly_cos_2 + weekly_sin_3 + weekly_cos_3,
-                    data=train_df_sub, tau = tau)
-        
-        result$Prediction = predict(model, test_df_sub) * test_df_sub$load_ratio
-        result$q = tau
-        
-        result_all[[counter]] = result
-        counter = counter + 1
+      result$Prediction = predict(model, test_df_sub) * test_df_sub$load_ratio
+      result$q = tau
+      
+      result_all_quantiles[[q_counter]] = result
+      q_counter = q_counter + 1
       }
-    }
+    rbindlist(result_all_quantiles)
   }
+  result_all[[iR]] = result_all_zones_hours
 }
 
 result_final = rbindlist(result_all)

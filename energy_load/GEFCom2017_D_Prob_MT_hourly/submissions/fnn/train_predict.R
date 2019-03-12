a406c89a14b0e50056f546b39a729eb7380b9477
Hong Lu <honglu@microsoft.com>
2019-03-12 18:30:39 -0400

    Added parallelization to fnn model.

--- energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/fnn/train_predict.R
+++ energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/fnn/train_predict.R
@@ -9,6 +9,13 @@ set.seed(seed_value)
 
 library('data.table')
 library('qrnn')
+library('doParallel')
+
+n_cores = detectCores()
+
+cl <- parallel::makeCluster(n_cores)
+parallel::clusterEvalQ(cl, lapply(c("quantreg", "data.table"), library, character.only = TRUE))
+registerDoParallel(cl)
 
 # Specify data directory
 data_dir = 'energy_load/GEFCom2017_D_Prob_MT_hourly/submissions/fnn/data/features'
@@ -26,7 +33,6 @@ quantiles = seq(0.1, 0.9, by = 0.1)
 
 # Train and predict
 result_all = list()
-counter = 1
 for (iR in 1:6){
   print(paste('Round', iR))
   train_file = file.path(train_dir, paste(train_file_prefix, iR, '.csv', sep=''))
@@ -44,6 +50,8 @@ for (iR in 1:6){
   
   zones = unique(train_df[, Zone])
   hours = unique(train_df[, hour_of_day])
+  all_zones_hours = expand.grid(zones, hours)
+  colnames(all_zones_hours) = c('Zone', 'hour_of_day')
 
   test_df$average_load_ratio = rowMeans(test_df[,c('recent_load_ratio_10', 'recent_load_ratio_11', 'recent_load_ratio_12',
                                                  'recent_load_ratio_13', 'recent_load_ratio_14', 'recent_load_ratio_15', 'recent_load_ratio_16')], na.rm=TRUE)
@@ -51,42 +59,43 @@ for (iR in 1:6){
 
   test_df[, load_ratio:=mean(average_load_ratio), by=list(hour_of_day, month_of_year)]
 
-  for (z in zones){
-    print(paste('Zone', z))
+  result_all_zones_hours = foreach(i = 1:nrow(all_zones_hours), .combine = rbind) %dopar%{
+    z = all_zones_hours[i, 'Zone']
+    h = all_zones_hours[i, 'hour_of_day']
+    train_df_sub = train_df[Zone == z & hour_of_day == h]
+    test_df_sub = test_df[Zone == z & hour_of_day == h]
     
-    for (h in hours){
-      train_df_sub = train_df[Zone == z & hour_of_day == h]
-      test_df_sub = test_df[Zone == z & hour_of_day == h]
-      
+    train_x <- as.matrix(train_df_sub[, c('load_lag', 'dry_bulb_lag',
+                           'annual_sin_1', 'annual_cos_1', 'annual_sin_2', 'annual_cos_2', 'annual_sin_3', 'annual_cos_3', 
+                           'weekly_sin_1', 'weekly_cos_1', 'weekly_sin_2', 'weekly_cos_2', 'weekly_sin_3', 'weekly_cos_3'), 
+                           drop=FALSE])
+    train_y <- as.matrix(train_df_sub[, c('DEMAND'), drop=FALSE])
+
+    test_x <- as.matrix(test_df_sub[, c('load_lag', 'dry_bulb_lag',
+                           'annual_sin_1', 'annual_cos_1', 'annual_sin_2', 'annual_cos_2', 'annual_sin_3', 'annual_cos_3', 
+                           'weekly_sin_1', 'weekly_cos_1', 'weekly_sin_2', 'weekly_cos_2', 'weekly_sin_3', 'weekly_cos_3'), 
+                           drop=FALSE])
+    
+    result_all_quantiles = list()
+    q_counter = 1
+    for (tau in quantiles){
       result = data.table(Zone=test_df_sub$Zone, Datetime = test_df_sub$Datetime, Round=iR)
+
+      model = qrnn2.fit(x=train_x, y=train_y, 
+                        n.hidden=8, n.hidden2=4,
+                        tau=tau, Th=tanh,
+                        iter.max=1,
+                        penalty=0)
+     
+      result$Prediction = qrnn2.predict(model, x=test_x) * test_df_sub$load_ratio
+      result$q = tau
       
-      train_x <- as.matrix(train_df_sub[, c('load_lag', 'dry_bulb_lag',
-                           'annual_sin_1', 'annual_cos_1', 'annual_sin_2', 'annual_cos_2', 'annual_sin_3', 'annual_cos_3', 
-                           'weekly_sin_1', 'weekly_cos_1', 'weekly_sin_2', 'weekly_cos_2', 'weekly_sin_3', 'weekly_cos_3'), 
-                           drop=FALSE])
-      train_y <- as.matrix(train_df_sub[, c('DEMAND'), drop=FALSE])
-
-      test_x <- as.matrix(test_df_sub[, c('load_lag', 'dry_bulb_lag',
-                           'annual_sin_1', 'annual_cos_1', 'annual_sin_2', 'annual_cos_2', 'annual_sin_3', 'annual_cos_3', 
-                           'weekly_sin_1', 'weekly_cos_1', 'weekly_sin_2', 'weekly_cos_2', 'weekly_sin_3', 'weekly_cos_3'), 
-                           drop=FALSE])
-
-      for (tau in quantiles){
-
-        model = qrnn2.fit(x=train_x, y=train_y, 
-                          n.hidden=8, n.hidden2=4,
-                          tau=tau, Th=tanh,
-                          iter.max=1,
-                          penalty=0)
-       
-        result$Prediction = qrnn2.predict(model, x=test_x) * test_df_sub$load_ratio
-        result$q = tau
-        
-        result_all[[counter]] = result
-        counter = counter + 1
-      }
+      result_all_quantiles[[q_counter]] = result
+      q_counter = q_counter + 1
     }
+    rbindlist(result_all_quantiles)
   }
+  result_all[[iR]] = result_all_zones_hours
 }
 
 result_final = rbindlist(result_all)

11abc272d1400b755495937d3738db2fad2e59c3
ZhouFang928 <ZhouFang928@users.noreply.github.com>
2019-03-25 13:36:03 +0800

    add script to source entire

---
+++ tests/unit/source_entire.R
@@ -0,0 +1,36 @@
+#!/usr/bin/Rscript 
+#
+# Source entire R testing files
+#
+# Note that we first define a function to source entire folder including R files. Then, 
+# we simply source all .R files within the specified folder.
+
+## Define a function to source entire folder
+source_entire_folder <- function(folderName, verbose=FALSE, showWarnings=TRUE) { 
+  # Find all .R files within a folder and soruces them
+  #
+  # Args:
+  #   folderName: Name of the folder including R files to be sourced.
+  #   verbose: If TRUE, print message; if not, not. Default is FALSE.
+  #
+  # Returns:
+  #   NULL.
+  files <- list.files(folderName, full.names=TRUE)
+
+  # Grab only R files
+  files <- files[ grepl("\\.[rR]$", files) ]
+
+  if (!length(files) && showWarnings)
+    warning("No R files in ", folderName)
+
+  for (f in files) {
+    if (verbose)
+      cat("sourcing: ", f, "\n")
+    ## TODO:  add caught whether error or not and return that
+    try(source(f, local=FALSE, echo=FALSE), silent=!verbose)
+  }
+  return(invisible(NULL))
+}
+
+## Source all .R files within the folder of test/unit
+source_entire_folder('./tests/unit')

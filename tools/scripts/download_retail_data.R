3c25bcf88ce363927a637007d8443036a112ede0
Vanja Paunic <15053814+vapaunic@users.noreply.github.com>
2020-01-22 11:04:06 +0000

    modified data download script

--- tools/scripts/download_retail_data.R
+++ tools/scripts/download_retail_data.R
@@ -1,18 +1,21 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-# Retrieves the orangeJuice dataset from the bayesm R package and saves as csv
+# This script retrieves the orangeJuice dataset from the bayesm R package and saves the data as csv
 
-install.packages("bayesm", repos = "http://mran.revolutionanalytics.com/snapshot/2018-08-27/")
-
+# Load the data
 library(bayesm)
-
 data("orangeJuice")
-
 yx <- orangeJuice[[1]]
 storedemo <- orangeJuice[[2]]
 
-fpath <- file.path("contrib", "tsperf", "OrangeJuice_Pt_3Weeks_Weekly", "data")
+# Create a data directory
+path_to_data <- "~/ojdata"
+fpath <- file.path(path_to_data)
+if(!dir.exists(fpath)) dir.create(fpath)
 
+# Write the data to csv files
 write.csv(yx, file = file.path(fpath, "yx.csv"), quote = FALSE, na = " ", row.names = FALSE)
 write.csv(storedemo, file = file.path(fpath, "storedemo.csv"), quote = FALSE, na = " ", row.names = FALSE)
+
+print(paste("Data download completed. Data saved to ", path_to_data))

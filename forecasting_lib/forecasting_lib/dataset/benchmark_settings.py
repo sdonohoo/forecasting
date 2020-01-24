8b7fef74eab09a775b6e43ddd45ce2e65fb9ee1d
Vanja Paunic <15053814+vapaunic@users.noreply.github.com>
2020-01-24 14:48:05 +0000

    removed paths from benchmark_settings

--- forecasting_lib/forecasting_lib/dataset/benchmark_settings.py
+++ forecasting_lib/forecasting_lib/dataset/benchmark_settings.py
@@ -3,15 +3,7 @@
 
 # This module contains benchmark related parameters.
 
-import os
 import pandas as pd
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-BENCHMARK_DIR = os.path.dirname(SCRIPT_DIR)
-TSPERF_DIR = os.path.dirname(os.path.dirname(BENCHMARK_DIR))
-
-SUBMISSIONS_DIR = os.path.join(BENCHMARK_DIR, "submissions")
-DATA_DIR = os.path.join(BENCHMARK_DIR, "data")
 
 NUM_ROUNDS = 12
 PRED_HORIZON = 3

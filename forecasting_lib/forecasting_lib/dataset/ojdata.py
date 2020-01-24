6f20b6cee6f03878c97e7fa2d412a3cf6f0b2a38
Vanja Paunic <15053814+vapaunic@users.noreply.github.com>
2020-01-24 14:24:21 +0000

    fixed split_train_test in ojdata.py

--- forecasting_lib/forecasting_lib/dataset/ojdata.py
+++ forecasting_lib/forecasting_lib/dataset/ojdata.py
@@ -3,9 +3,6 @@
 
 import os
 import subprocess
-
-import inspect
-import argparse
 import pandas as pd
 
 import forecasting_lib.dataset.benchmark_settings as bs
@@ -51,15 +48,13 @@ def maybe_download(dest_dir):
         print("Data already exists at the specified location.")
 
 
-def serve_folds(write_csv=False):
+def split_train_test(data_dir, write_csv=False):
     """Generate training, testing, and auxiliary datasets. Training data includes the historical 
     sales and external features; testing data contains the future sales and external features; 
     auxiliary data includes the future price, deal, and advertisement information which can be 
     used for making predictions (we assume such auxiliary information is available at the time 
     when we generate the forecasts).
 
-    Return training and testing data for each forecast round
-    
     You can use this script in either of the following two ways
     1. Import the serve_folds module from this script to generate the training and testing data for
     each forecast period on the fly
@@ -76,18 +71,37 @@ def serve_folds(write_csv=False):
     generating forecasts in each round. However, test_*.csv files in /test folder can only be used
     for model performance evaluation.
 
+    Example:
+        if __name__ == "__main__":
+            # Test serve_folds
+            parser = argparse.ArgumentParser()
+            parser.add_argument("--test", help="Run the test of serve_folds function", action="store_true")
+            parser.add_argument("--save", help="Write training and testing data into csv files", action="store_true")
+            parser.add_argument("--data-dir", help="The location of the downloaded oj data")
+            args = parser.parse_args()
+
+            if args.test:
+                for train, test, aux in split_train_test(data_dir=args.data_dir, write_csv=args.save):
+                    print("Training data size: {}".format(train.shape))
+                    print("Testing data size: {}".format(test.shape))
+                    print("Auxiliary data size: {}".format(aux.shape))
+                    print("Minimum training week number: {}".format(min(train["week"])))
+                    print("Maximum training week number: {}".format(max(train["week"])))
+                    print("Minimum testing week number: {}".format(min(test["week"])))
+                    print("Maximum testing week number: {}".format(max(test["week"])))
+                    print("Minimum auxiliary week number: {}".format(min(aux["week"])))
+                    print("Maximum auxiliary week number: {}".format(max(aux["week"])))
+                    print("")
+
     Args:
         write_csv (Boolean): Whether to write the data files or not
     """
-    # Get the directory of this script and directory of the OrangeJuice dataset
-    SCRIPT_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    DATA_DIR = os.path.join(os.path.dirname(SCRIPT_DIR), "data")
     # Read sales data into dataframe
-    sales = pd.read_csv(os.path.join(DATA_DIR, "yx.csv"), index_col=0)
+    sales = pd.read_csv(os.path.join(data_dir, "yx.csv"), index_col=0)
 
     if write_csv:
-        TRAIN_DATA_DIR = os.path.join(DATA_DIR, "train")
-        TEST_DATA_DIR = os.path.join(DATA_DIR, "test")
+        TRAIN_DATA_DIR = os.path.join(data_dir, "train")
+        TEST_DATA_DIR = os.path.join(data_dir, "test")
         if not os.path.isdir(TRAIN_DATA_DIR):
             os.mkdir(TRAIN_DATA_DIR)
         if not os.path.isdir(TEST_DATA_DIR):
@@ -106,23 +120,3 @@ def serve_folds(write_csv=False):
             test.to_csv(os.path.join(TEST_DATA_DIR, "test_round_" + str(i + 1) + ".csv"))
             aux.to_csv(os.path.join(TRAIN_DATA_DIR, "aux_round_" + str(i + 1) + ".csv"))
         yield train, test, aux
-
-
-if __name__ == "__main__":
-    # Test serve_folds
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--test", help="Run the test of serve_folds function", action="store_true")
-    parser.add_argument("--save", help="Write training and testing data into csv files", action="store_true")
-    args = parser.parse_args()
-    if args.test:
-        for train, test, aux in serve_folds(args.save):
-            print("Training data size: {}".format(train.shape))
-            print("Testing data size: {}".format(test.shape))
-            print("Auxiliary data size: {}".format(aux.shape))
-            print("Minimum training week number: {}".format(min(train["week"])))
-            print("Maximum training week number: {}".format(max(train["week"])))
-            print("Minimum testing week number: {}".format(min(test["week"])))
-            print("Maximum testing week number: {}".format(max(test["week"])))
-            print("Minimum auxiliary week number: {}".format(min(aux["week"])))
-            print("Maximum auxiliary week number: {}".format(max(aux["week"])))
-            print("")

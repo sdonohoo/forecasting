487d6c35b9bb2a0ede4270de3c24a258ca7f58a4
Chenhui Hu <chenhhu@microsoft.com>
2020-01-28 14:02:27 -0500

    Chenhui/light gbm quick start (#40)
    * initial example notebook for lightgbm
    
    * reduced to one round forecast
    
    * added text
    
    * added text
    
    * added text
    
    * moved week_of_month to feature engineering utils
    
    * moved df_from_cartesian_product to feature utils
    
    * moved functions to feature utils
    
    * moved functions to feature utils
    
    * added lightgbm model utils
    
    * updated plots
    
    * added text and renamed predict function
    
    * reduced print out frequency in model training
    
    * moved data visualization code to utils
    
    * added text
    
    * updated plot function and added docstring
    
    * renamed the notebook
    
    * updated text

--- forecasting_lib/forecasting_lib/feature_engineering/feature_utils.py
+++ forecasting_lib/forecasting_lib/feature_engineering/feature_utils.py
@@ -2,9 +2,9 @@
 # Licensed under the MIT License.
 
 """
-This file contains helper functions for creating features for TSPerf
-reference implementations and submissions. All functions defined assume 
-that there is no missing data.
+This file contains utility functions for creating features for time
+series forecasting applications. All functions defined assume that
+there is no missing data.
 """
 
 from datetime import timedelta
@@ -104,6 +104,31 @@ def time_of_year(datetime_col):
 def week_of_year(datetime_col):
     """Returns the week from a datetime column."""
     return datetime_col.dt.week
+
+
+def week_of_month(date_time):
+    """Returns the week of the month for a specified date.
+
+    Args:
+        dt (Datetime): Input date
+
+    Returns:
+        wom (Integer): Week of the month of the input date
+    """
+
+    def _week_of_month(date_time):
+        from math import ceil
+
+        first_day = date_time.replace(day=1)
+        dom = date_time.day
+        adjusted_dom = dom + first_day.weekday()
+        wom = int(ceil(adjusted_dom / 7.0))
+        return wom
+
+    if isinstance(date_time, pd.Series):
+        return date_time.apply(lambda x: _week_of_month(x))
+    else:
+        return _week_of_month(date_time)
 
 
 def month_of_year(date_time_col):
@@ -804,3 +829,77 @@ def same_day_hour_moving_agg(
     df.drop(["fct_diff", "value"], inplace=True, axis=1)
 
     return df
+
+
+def df_from_cartesian_product(dict_in):
+    """Generate a Pandas dataframe from Cartesian product of lists.
+    
+    Args: 
+        dict_in (Dictionary): Dictionary containing multiple lists, e.g. {"fea1": list1, "fea2": list2}
+        
+    Returns:
+        df (Dataframe): Dataframe corresponding to the Caresian product of the lists
+    """
+    from itertools import product
+
+    cart = list(product(*dict_in.values()))
+    df = pd.DataFrame(cart, columns=dict_in.keys())
+    return df
+
+
+def lagged_features(df, lags):
+    """Create lagged features based on time series data.
+    
+    Args:
+        df (Dataframe): Input time series data sorted by time
+        lags (List): Lag lengths
+        
+    Returns:
+        fea (Dataframe): Lagged features 
+    """
+    df_list = []
+    for lag in lags:
+        df_shifted = df.shift(lag)
+        df_shifted.columns = [x + "_lag" + str(lag) for x in df_shifted.columns]
+        df_list.append(df_shifted)
+    fea = pd.concat(df_list, axis=1)
+    return fea
+
+
+def moving_averages(df, start_step, window_size=None):
+    """Compute averages of every feature over moving time windows.
+    
+    Args:
+        df (Dataframe): Input features as a dataframe
+        start_step (Integer): Starting time step of rolling mean
+        window_size (Integer): Windows size of rolling mean
+    
+    Returns:
+        fea (Dataframe): Dataframe consisting of the moving averages
+    """
+    if window_size is None:
+        # Use a large window to compute average over all historical data
+        window_size = df.shape[0]
+    fea = df.shift(start_step).rolling(min_periods=1, center=False, window=window_size).mean()
+    fea.columns = fea.columns + "_mean"
+    return fea
+
+
+def combine_features(df, lag_fea, lags, window_size, used_columns):
+    """Combine lag features, moving average features, and orignal features in the data.
+    
+    Args:
+        df (Dataframe): Time series data including the target series and external features
+        lag_fea (List): A list of column names for creating lagged features
+        lags (Numpy Array): Numpy array including all the lags
+        window_size (Integer): Window size of rolling mean
+        used_columns (List): A list containing the names of columns that are needed in the 
+        input dataframe (including the target column)
+    
+    Returns:
+        fea_all (Dataframe): Dataframe including all the features
+    """
+    lagged_fea = lagged_features(df[lag_fea], lags)
+    moving_avg = moving_averages(df[lag_fea], 2, window_size)
+    fea_all = pd.concat([df[used_columns], lagged_fea, moving_avg], axis=1)
+    return fea_all

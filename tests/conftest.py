23003b9c541e4c16cb0b6d19e8f1ce15fc3e8e50
Ubuntu <chenhhu@microsoft.com>
2020-02-03 19:30:08 +0000

    added integration test

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+import pytest
+from forecasting_lib.common.utils import git_repo_path
+
+
+@pytest.fixture(scope="module")
+def notebooks():
+    repo_path = git_repo_path()
+    examples_path = os.path.join(repo_path, "examples")
+    quick_start_path = os.path.join(examples_path, "00_quick_start")
+
+    # Path for the notebooks
+    paths = {"lightgbm_quick_start": os.path.join(quick_start_path, "lightgbm_point_forecast.ipynb")}
+    return paths
